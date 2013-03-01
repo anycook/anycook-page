9cc318b62e4b9a44eaac02a7a3395242e334e1fa
Jan Grassegger <jan@anycook.de>
2013-03-01 23:15:04 +0100

    added jquery.recipeoverview and included it in profiles and discover

--- WebContent/scripts/userProfile.js
+++ WebContent/scripts/userProfile.js
@@ -54,36 +54,22 @@ function loadProfile(userid){
 		$(".profile_search").attr("href", "#!/search/user/"+encodeURIComponent(profileData.name));
 		
 		User.getRecipes(userid, function(recipes){
-			$(".profile_achievements .recipes .count").text(recipes.total);
-			
-			if(recipes.total>0){
-				var $recipes = $("#profile_recipes").show();
-				$recipes.children("h2").text("Rezepte von "+profileData.name+" ("+recipes.total+")");
-				var $p = $recipes.children("p");
-				for(var i in recipes.names){
-					$p.append(profileRecipe(recipes.names[i]));
-				}
-				if(recipes.total<=5)
-					$("#profile_recipes p").css("height", 120);
-				
-				if(recipes.total>10)
-					$("#profile_recipes .profile_more").show();
-			}
+			var headline;
+			if(userid == user.id)
+				headline = "Deine Rezepte ("+recipes.total+")";
+			else
+				headline = "Rezepte von "+profileData.name+" ("+recipes.total+")";
+			$("#profile_recipes").recipeoverview(headline, recipes.names).show();
 		});
 		
 		$.anycook.graph.user.schmeckt(userid, function(schmeckt){
-			$(".profile_achievements .likes .count").text(schmeckt.length);
-			var $schmeckt = $("#profile_schmeckt").show();
-			$schmeckt.children("h2").text("Lieblingsrezepte von "+profileData.name+" ("+schmeckt.length+")");
-			var $p = $schmeckt.children("p");
-			for(var i in schmeckt){
-				$p.append(profileRecipe(schmeckt[i]));
-			}
-			if(schmeckt.length<=5)
-				$("#profile_schmeckt p").css("height", 120);
-			
-			if(schmeckt.length>10)
-				$("#profile_schmeckt .profile_more").show();
+			var headline;
+
+			if(userid == user.id)
+				headline = "Deine Lieblingsrezepte ("+schmeckt.length+")";
+			else
+				headline = "Lieblingsrezepte von "+profileData.name+" ("+schmeckt.length+")";
+			$("#profile_schmeckt").recipeoverview(headline, schmeckt).show();
 		});
 
 		
@@ -92,23 +78,12 @@ function loadProfile(userid){
 			$(".profile_achievements .discussions .count").text(discussionNum);
 		});
 	});
-	
-	$(".profile_more").click(profileShowMore);
 		
 	if(userid == user.id){
 		$.anycook.graph.user.recommendations (function(json){
         	if(json.length>0){
-        		var $recommendation = $("#profile_recommendation").show();
-        		$recommendation.children("h2").text("Diese Rezepte könnten dir auch schmecken");
-        		var $p = $recommendation.children("p");
-	        	for(var i = 0; i<json.length && i<20; i++){
-	        		$p.append(profileRecipe(json[i]));
-	        	}
-	        	if($p.children("a").length<=5)
-	    			$p.css("height", 120);
-	    		
-	    		if($p.children("a").length>10)
-	    			$recommendation.children(".profile_more").show();
+        		var headline = "Diese Rezepte könnten dir auch schmecken";
+        		$("#profile_recommendation").recipeoverview(headline, json).show();
         	}
 		});
 	}
@@ -136,27 +111,4 @@ function follow(){
 
 		$follower.text(numFollowers-1);
 	}
-}
-
-
-function profileShowMore(event){
-	var newheight;
-	var p = $(this).siblings("p").first();
-	if($(this).text() == "mehr anzeigen"){
-		var numelements = p.children().length;	
-		
-		var rest = numelements % 5;
-		newheight = ((numelements-rest)/5)*120;
-		if(rest>0) newheight+=120;
-		
-		$(this).text("weniger anzeigen");
-		/*$(this).animate({height:0, opacity:0},{duration:1000, complete:function(){
-			$(this).remove();
-			}});*/
-	}else{
-		newheight = 240;
-		$(this).text("mehr anzeigen");
-	}
-	
-	p.animate({height:newheight}, 1000);
 }
