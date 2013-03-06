bb1dc9e6dbe56b35e8aba49c4d8401e6d665bd79
Jan Graßegger <jan@anycook.de>
2013-03-06 19:40:19 +0100

    fixed FBlogin

--- WebContent/scripts/facebook.js
+++ WebContent/scripts/facebook.js
@@ -1,21 +1,23 @@
 function fbLogin(){
 	FB.login(function(response) {
-	    if (response.session) {
-	    	if(user.checkLogin()){
-	    		/*if($.address.pathNames().length == 0 || $.address.pathNames()[0] == "home"){
-	    			user = User.init();
-	    			makeUsermenuText();
-	    			$("#login_dropdown").hide();
-	    		}
-	    		else
-	    			window.location.reload();*/
-	    	}
-	    	else{
-	    		$("#login_dropdown").hide();
-	    		$("#signin_btn").removeClass("on");
-	    		makeFBkRegistrationPopup();
-	    	}
-	    	
+	    if (response.authResponse) {
+	    	$.when(User.init()).then(function(userinit){
+	    		user = userinit;
+	    		if(user.checkLogin()){
+		    		// if($.address.pathNames().length == 0 || $.address.pathNames()[0] == "home"){
+		    		// 	user = User.init();
+		    		// 	makeUsermenuText();
+		    		// 	$("#login_dropdown").hide();
+		    		// }
+		    		// else
+		    			window.location.reload();
+		    	}
+		    	else{
+		    		$("#login_dropdown").hide();
+		    		$("#signin_btn").removeClass("on");
+		    		makeFBkRegistrationPopup();
+		    	}
+	    	});	    	
 	    } else {
 	      // TODO The user has logged out, and the cookie has been cleared
 	    	//alert("false");
