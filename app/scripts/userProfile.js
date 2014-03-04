a1b9478308f563c8a10d74bf4e5e8e2f4fba6f68
Jan Graßegger <krankgesund@googlemail.com>
2014-03-04 12:53:59 +0100

    fixed user recommendations

--- app/scripts/userProfile.js
+++ app/scripts/userProfile.js
@@ -1,20 +1,20 @@
 /**
  * @license This file is part of anycook. The new internet cookbook
  * Copyright (C) 2014 Jan Graßegger
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see [http://www.gnu.org/licenses/].
- * 
+ *
  * @author Jan Graßegger <jan@anycook.de>
  */
 define([
@@ -61,7 +61,7 @@ define([
 
 				var dateString = date.getDateString(profileData.date);
 				$('.profile_date span').text(dateString);
-				
+
 				$('.profile_buttons #showRecipes').attr('href', '#/search?user='+userid);
 
 				if(user.checkLogin()){
@@ -73,7 +73,7 @@ define([
 							$('#follow').text('- Entfolgen').addClass('on');
 						}
 
-						//lightbox	
+						//lightbox
 						$('#sendmessage').show().click(function(){
 							var $lightbox = messageStream.getNewMessageLightbox();
 							var top = $(this).offset().top-313;
@@ -85,37 +85,37 @@ define([
 						});
 					} else{
 						$('.profile_buttons #preferences').show();
-						
+
 					}
 
 					//$('.profile_buttons').show();
-					
+
 				}
 
 
-				
+
 				if(profileData.place !== null){
 					$('.profile_place').show().children('span').text(profileData.place);
 				}
-				
+
 				$('.profile_achievements .follower .count').text(profileData.followers.length);
-				
+
 				if(profileData.text !== null && profileData.text.length > 0){
 					$('.profile_text').show().text('»'+profileData.text+'«');
 				}
-				
+
 				// if(profileData.facebook_id>0){
 				// 	var fblink = $('.profile_facebook');
 				// 	fblink.attr('href', profileData.getFacebookProfileLink());
-				// 	fblink.css('display', 'block');		
+				// 	fblink.css('display', 'block');
 				// }
-				
+
 				$('.profile_search').attr('href', '#!/search/user/'+encodeURIComponent(profileData.name));
-				
+
 				User.getRecipes(userid, function(recipes){
 					$('.profile_achievements .recipes .count').text(recipes.length);
 					if(recipes.length > 0){
-						
+
 						var headline;
 						if(userid === user.id){
 							headline = 'Deine Rezepte ('+recipes.length+')';
@@ -126,7 +126,7 @@ define([
 						$('#profile_recipes').recipeoverview(headline, recipes).show();
 					}
 				});
-				
+
 				AnycookAPI.user.schmeckt(userid, function(schmeckt){
 					$('.profile_achievements .likes .count').text(schmeckt.length);
 					if(schmeckt.length > 0){
@@ -142,22 +142,24 @@ define([
 					}
 				});
 
-				
-				
+
+
 				User.getDiscussionNum(userid, function(discussionNum){
 					$('.profile_achievements .discussions .count').text(discussionNum);
 				});
+
+                if(userid === user.id){
+                    AnycookAPI.user.recommendations (function(json){
+                        if(json.length>0){
+                            var headline = 'Diese Rezepte könnten dir auch schmecken';
+                            $('#profile_recommendation').recipeoverview(headline, json).show();
+                        }
+                    });
+                }
 			});
-				
-			if(userid === user.id){
-				AnycookAPI.user.recommendations (function(json){
-					if(json.length>0){
-						var headline = 'Diese Rezepte könnten dir auch schmecken';
-						$('#profile_recommendation').recipeoverview(headline, json).show();
-					}
-				});
-			}
-			
+
+
+
 		},
 		gotoProfile : function(username){
 			$.address.value('profile/'+encodeURIComponent(username));
